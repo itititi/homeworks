dd42cbd7355796e2c2abeef28f41ca5ea0d7e608
ramp <1@gmail.com>
2023-01-18 23:40:15 +0300

    new dz

--- 2.3_SQL/SELECT.sql
+++ 2.3_SQL/SELECT.sql
@@ -1,6 +1,6 @@
 /* Задача 1 */
 SELECT name, date_created FROM albums
-WHERE date_created BETWEEN '2018-01-01' AND '2018-12-31';
+WHERE date_created  BETWEEN '2018-01-01' AND '2018-12-31';
 
 /* Задача 2 */
 SELECT name, time FROM tracks
