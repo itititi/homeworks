6c5bcdc0f8eff05411f823f85264edeb22bea123
SISKI21 <roman915@bk.ru>
2023-05-24 13:41:19 +0300

    new dz

--- 4.7_permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 4.7_permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -76,7 +76,15 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        # 'rest_framework.throttling.UserRateThrottle',
+        # 'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10/minute',
+        'anon': '2/minute'
+}
 }
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
